9201c8cbed6cfffac78b6434c760eb0453b7e8bc
Tran Dat <Tran Dat@DESKTOP-SLFVQSH>
2019-01-08 09:53:17 +0700

    create thongtinbylatlngviewmodel

---
+++ GisTest/ViewModels/ThongTinByLatLngViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GisTest.ViewModels
+{
+    public class ThongTinByLatLngViewModel
+    {
+        public string Id { get; set; }
+        public string DuLieuDoiTuong { get; set; }
+        public string Value { get; set; }
+    }
+}
