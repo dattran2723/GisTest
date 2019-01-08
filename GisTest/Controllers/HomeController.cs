e03777db8aefe0bdfbf94b52024b2296825b0b6c
Tran Dat <Tran Dat@DESKTOP-SLFVQSH>
2019-01-08 09:30:30 +0700

    update models

--- GisTest/Controllers/HomeController.cs
+++ GisTest/Controllers/HomeController.cs
@@ -14,40 +14,17 @@ namespace GisTest.Controllers
     public class HomeController : Controller
     {
         private GisData db = new GisData();
+        private ThongTinDoiTuongChinh doiTuongChinh = new ThongTinDoiTuongChinh();
+
         public ActionResult Index()
         {
-            return View(GetDoiTuongChinhByDiaGioiHanhChinhCode("001"));
+            return View(doiTuongChinh.GetDoiTuongChinhByDiaGioiHanhChinhCode("001"));
         }
-
-        /// <summary>
-        /// Lấy thông tin đối tượng chỉnh bởi "DiaGioiHanhChinhCode"
-        /// </summary>
-        /// <param name="value">
-        /// </param>
-        /// <returns>
-        /// danh sách các đối tượng có "DiaGioiHanhChinhCode" = value
-        /// </returns>
-        public List<ObjectViewModel> GetDoiTuongChinhByDiaGioiHanhChinhCode(string value)
-        {
-            IQueryable<ObjectViewModel> list = from a in db.ThongTinDoiTuongChinhs
-                                               join b in db.ThongTinDoiTuongPhus
-                                               on a.Id equals b.ThongTinDoiTuongChinhId
-                                               where a.DiaGioiHanhChinhCode == value
-                                               orderby a.Ten ascending
-                                               select new ObjectViewModel()
-                                               {
-                                                   Id = a.Id,
-                                                   Ten = a.Ten,
-                                                   Value = b.Value,
-                                                   Lat = a.Lat,
-                                                   Lng = a.Lng
-                                               };
-            return list.ToList();
-        }
+        
 
         public JsonResult GetDiaDiem(string value)
         {
-            return Json(GetDoiTuongChinhByDiaGioiHanhChinhCode(value), JsonRequestBehavior.AllowGet);
+            return Json(doiTuongChinh.GetDoiTuongChinhByDiaGioiHanhChinhCode(value), JsonRequestBehavior.AllowGet);
         }
 
         /// <summary>
@@ -58,7 +35,7 @@ namespace GisTest.Controllers
         public JsonResult GetFullThongTinDoiTuongByValue(string value)
         {
             List<ObjectViewModel> listObj = new List<ObjectViewModel>();
-            listObj.Add(GetThongTinDoiTuongByValue(value));
+            listObj.Add(doiTuongChinh.GetThongTinDoiTuongByValue(value));
             return Json(GetThongTinDoiTuongCha(listObj), JsonRequestBehavior.AllowGet);
         }
 
@@ -72,40 +49,14 @@ namespace GisTest.Controllers
             ObjectViewModel pa = model[model.Count - 1];
             if (pa != null)
             {
-                model.Add(GetThongTinDoiTuongByValue(pa.DiaGioiHanhChinhCode));
+                model.Add(doiTuongChinh.GetThongTinDoiTuongByValue(pa.DiaGioiHanhChinhCode));
                 return GetThongTinDoiTuongCha(model);
             }
             model.Remove(pa);
             return model;
         }
 
-        /// <summary>
-        /// Lấy thông tin đối tượng bởi value
-        /// </summary>
-        /// <param name="value"></param>
-        /// <returns>
-        /// Đối tượng có value = value
-        /// </returns>
-        public ObjectViewModel GetThongTinDoiTuongByValue(string value)
-        {
-            IQueryable<ObjectViewModel> info = from a in db.ThongTinDoiTuongChinhs
-                                               join b in db.ThongTinDoiTuongPhus on a.Id equals b.ThongTinDoiTuongChinhId
-                                               join c in db.ThongTinVeDoiTuongs on a.Id equals c.ThongTinDoiTuongChinhId
-                                               where b.Value == value
-                                               select new ObjectViewModel
-                                               {
-                                                   Id = a.Id,
-                                                   Ten = a.Ten,
-                                                   Value = b.Value,
-                                                   Code = b.Code,
-                                                   Lat = a.Lat,
-                                                   Lng = a.Lng,
-                                                   DuLieuVe = c.DuLieuDoiTuong,
-                                                   DiaGioiHanhChinhCode = a.DiaGioiHanhChinhCode,
-                                                   Zoom = (b.Code == "XA/PHUONG" ? 12 : (b.Code == "HUYEN/QUAN" ? 11 : 9))
-                                               };
-            return info.FirstOrDefault();
-        }
+        
         /// <summary>
         /// truyen vao 2 gia tri Lat, Lng
         /// dem so sanh voi cac gia tri Max Min bang cach goi store
@@ -129,18 +80,6 @@ namespace GisTest.Controllers
                     result = item.Value;
                 }
             }
-            return Json(result, JsonRequestBehavior.AllowGet);
-
-            //foreach (var item in res)
-            //{
-            //    List<Point> listPoint = GetDuLieuDoiTuong(item.DuLieuDoiTuong);
-            //    Point point = new Point(Lng, Lat);
-            //    var kqtrave = point.IsPointInPolygon(listPoint);
-            //    if (kqtrave == true)
-            //    {
-            //        result = item.Value;
-            //    }
-            //}
             return Json(result, JsonRequestBehavior.AllowGet);
 
         }
--- GisTest/Models/ThongTinDoiTuongChinh.cs
+++ GisTest/Models/ThongTinDoiTuongChinh.cs
@@ -1,10 +1,12 @@
-namespace GisTest.Models
+﻿namespace GisTest.Models
 {
+    using GisTest.ViewModels;
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
+    using System.Linq;
 
     [Table("ThongTinDoiTuongChinh")]
     public partial class ThongTinDoiTuongChinh
@@ -50,5 +52,62 @@ namespace GisTest.Models
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<ThongTinVeDoiTuong> ThongTinVeDoiTuongs { get; set; }
+
+        private GisData db = new GisData();
+
+
+        /// <summary>
+        /// Lấy thông tin đối tượng chỉnh bởi "DiaGioiHanhChinhCode"
+        /// </summary>
+        /// <param name="value">
+        /// </param>
+        /// <returns>
+        /// danh sách các đối tượng có "DiaGioiHanhChinhCode" = value
+        /// </returns>
+        public List<ObjectViewModel> GetDoiTuongChinhByDiaGioiHanhChinhCode(string value)
+        {
+            IQueryable<ObjectViewModel> list = from a in db.ThongTinDoiTuongChinhs
+                                               join b in db.ThongTinDoiTuongPhus
+                                               on a.Id equals b.ThongTinDoiTuongChinhId
+                                               where a.DiaGioiHanhChinhCode == value
+                                               orderby a.Ten ascending
+                                               select new ObjectViewModel()
+                                               {
+                                                   Id = a.Id,
+                                                   Ten = a.Ten,
+                                                   Value = b.Value,
+                                                   Lat = a.Lat,
+                                                   Lng = a.Lng
+                                               };
+            return list.ToList();
+        }
+
+        /// <summary>
+        /// Lấy thông tin đối tượng bởi value
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>
+        /// Đối tượng có value = value
+        /// </returns>
+        public ObjectViewModel GetThongTinDoiTuongByValue(string value)
+        {
+            IQueryable<ObjectViewModel> info = from a in db.ThongTinDoiTuongChinhs
+                                               join b in db.ThongTinDoiTuongPhus on a.Id equals b.ThongTinDoiTuongChinhId
+                                               join c in db.ThongTinVeDoiTuongs on a.Id equals c.ThongTinDoiTuongChinhId
+                                               where b.Value == value
+                                               select new ObjectViewModel
+                                               {
+                                                   Id = a.Id,
+                                                   Ten = a.Ten,
+                                                   Value = b.Value,
+                                                   Code = b.Code,
+                                                   Lat = a.Lat,
+                                                   Lng = a.Lng,
+                                                   DuLieuVe = c.DuLieuDoiTuong,
+                                                   DiaGioiHanhChinhCode = a.DiaGioiHanhChinhCode,
+                                                   Zoom = (b.Code == "XA/PHUONG" ? 12 : (b.Code == "HUYEN/QUAN" ? 11 : 9))
+                                               };
+            return info.FirstOrDefault();
+        }
     }
 }
