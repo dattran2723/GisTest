829d8f641ce9e78d63fcacc73dc14d105171f065
long đoàn <doanthanhlong1801@gmail.com>
2019-01-09 08:58:17 +0700

    comment code

--- GisTest/Controllers/HomeController.cs
+++ GisTest/Controllers/HomeController.cs
@@ -16,8 +16,12 @@ namespace GisTest.Controllers
         {
             return View(doiTuongChinh.GetDoiTuongChinhByDiaGioiHanhChinhCode("001"));
         }
-        
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value">truyền vào giá trị DiaGioiHanhChinhCode</param>
+        /// <returns>trả về danh sách đối tượng con của DiaGioiHanhChinhCode đó</returns>
         public JsonResult GetDiaDiem(string value)
         {
             return Json(doiTuongChinh.GetDoiTuongChinhByDiaGioiHanhChinhCode(value), JsonRequestBehavior.AllowGet);
@@ -25,10 +29,11 @@ namespace GisTest.Controllers
 
 
         /// <summary>
-        /// Lấy đầy đủ một thông tin đối tượng bởi value
+        /// Lấy đầy đủ một thông tin đối tượng bởi value truyền vào
+        /// Nếu kích vào phường hay quận thì sẻ lấy đến thông tin của đối tượng cha nó
         /// </summary>
-        /// <param name="value"></param>
-        /// <returns></returns>
+        /// <param name="value">truyền vào giá trị DiaGioiHanhChinhCode</param>
+        /// <returns>trả về danh sách thông tin đối tượng cha và con</returns>
         public JsonResult GetFullThongTinDoiTuongByValue(string value)
         {
             List<ObjectViewModel> listObj = new List<ObjectViewModel>();
@@ -39,7 +44,7 @@ namespace GisTest.Controllers
         /// <summary>
         /// Khởi tạo biến result
         /// Gọi hàm IsPointInPolygon() để kiểm trả có phải điểm đó nằm trong Polygon hay không
-        /// Nếu có thì sẻ gán giá trị Value vào resultValue
+        /// Nếu có thì sẻ gán giá trị item.Value vào result
         /// </summary>
         /// <param name="Lat">Truyền vào giá trị Lat</param>
         /// <param name="Lng">Truyền vào giá trị Lng</param>
@@ -62,22 +67,28 @@ namespace GisTest.Controllers
 
         }
         /// <summary>
-        /// select tung phan tu trong 'coordinates' va add vao List<Point>
+        /// Lấy các giá trị trong  "coordinates" của chuổi polygon đó và thêm từng điểm Point(X,Y) vào list Point
         /// </summary>
-        /// <param name="duLieuDoiTuong">la chuoi DuLieuDoiTuong </param>
-        /// <returns>list cac point cua polygon</returns>
+        /// <param name="duLieuDoiTuong">truyền chuổi DuLieuDoiTuong </param>
+        /// <returns>danh sách các điểm Point trong polygon đó</returns>
         public List<Point> GetPolygonFromDuLieuDoiTuong(string duLieuDoiTuong)
         {
             JObject json = JObject.Parse(duLieuDoiTuong);
             var geometry = json.SelectToken("geometry");
+            string ktra = (string)geometry.SelectToken("type");
+            if(ktra == "MultiPolygon")
+            {
+            }
             JArray coordinates = (JArray)geometry.SelectToken("coordinates")[0];
             List<Point> points = new List<Point>();
+            var c = 0;
             foreach (var item in coordinates)
             {
-                var x = item[0];
-                var y = item[1];
-                Point point = new Point((double)x, (double)y);
-                points.Add(point);
+                c++;
+                //var x = item[0];
+                //var y = item[1];
+                //Point point = new Point((double)x, (double)y);
+                //points.Add(point);
             }
             return points;
         }
